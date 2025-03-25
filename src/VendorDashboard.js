325bab4f1edd6881cde4e893d3f4b46a19e985e6
Mariya004 <mariyajyothy04@gmail.com>
2025-03-25 18:50:13 +0530

    modified vendor dashboard UI

--- src/VendorDashboard.js
+++ src/VendorDashboard.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { motion } from "framer-motion";
+
 
 export default function VendorDashboard() {
   const [userData, setUserData] = useState(null);
