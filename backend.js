808754278b4abdc86e646f8fd33ef762a8f790ec
Mariya004 <mariyajyothy04@gmail.com>
2025-04-23 17:11:26 +0530

    changes made in Login.js to prevent incorrect role login

--- backend.js
+++
@@ -1,5 +0,0 @@
-        res.status(200).json({
-            status: "success",
-            message: "Login successful",
-            data: { user_id: user._id, user_type: userType, token, role: userType },
-        });
